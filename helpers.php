fce5a13a7a2438e65ce36fe285f424d441cf481e
johnclendvoy <johntheleatherman@gmail.com>
2017-04-12 12:47:39 -0600

    created helpers.php
    made

---
+++ helpers.php
@@ -0,0 +1,32 @@
+/*
+* Show the style that a tab is selected based on the url
+*/
+function setActiveTab($tab_name)
+{
+	if(Request::segment(1) == $tab_name)
+	{
+		return 'active open';
+	}
+	else{
+		return '';
+	}
+}
+
+/**
+* Boolean to Yes\No string
+*/
+function b2yn($bool)
+{
+	if($bool == 0 || $bool == '0')
+	{
+		return 'No';
+	}
+	else if($bool == 1 || $bool == '1')
+	{
+		return 'Yes';
+	}
+	else
+	{
+		return $bool;
+	}
+}
